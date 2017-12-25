4c4b9b5c56bca44c4283ff3b295bd3cfa00abb53
p-Mart <phillip.martin012@gmail.com>
2017-12-25 17:21:04 -0500

    Testing on taxi environment. Improved metrics. Added some command line options. Rewards during training are saved now.

--- MQN.py
+++ MQN.py
@@ -1,7 +1,9 @@
-import sys, os
+import sys, os, errno
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 from keras.models import load_model
 from keras.optimizers import RMSprop, Adam
@@ -12,6 +14,7 @@ from rl.agents.dqn import DQNAgent, DistributionalDQNAgent
 from rl.policy import EpsGreedyQPolicy, LinearAnnealedPolicy
 from rl.memory import SequentialMemory, Memory
 from rl.callbacks import TrainEpisodeLogger
+from rl.core import Processor
 
 import gym
 from MQNModel import MQNmodel, DistributionalMQNModel
@@ -21,6 +24,29 @@ from DQNModel import DQNmodel
 #Need better metrics (percent of episodes)
 #Need model saving / loading
 #Need model visualization ability
+class TaxiProcessor(Processor):
+    def process_observation(self, observation):
+        processed_observation = observation / 500.
+        return processed_observation
+
+    def process_state_batch(self, batch):
+        processed_batch = batch / 500.
+        return processed_batch
+
+def outputLayer(model, sample):
+    inp = model.input                                           # input placeholder
+    outputs = [layer.output for layer in model.layers]          # all layer outputs
+    functor = K.function([inp]+ [K.learning_phase()], outputs ) # evaluation function
+
+    # Testing
+    layer_outs = functor([sample, 1.])
+
+    for idx, layer in enumerate(layer_outs):
+        print "\nLayer {}:".format(idx)
+        print "Shape: ", layer.shape
+        print layer
+
+    #print layer_outs
 
 def visualizeLayer(model, layer, sample):
     inputs = [K.learning_phase()] + model.inputs
@@ -46,18 +72,20 @@ def visualizeLayer(model, layer, sample):
     #print output
     #print "Output shape: ", output.shape
 
+def logmetrics(log, model_name):
 
-
-
-def showmetrics(log):
     ##### Metrics #####
+    episode_rewards = []
     episodic_reward_means = []
     episodic_losses = []
     episodic_mean_q = []
 
+    #Iterate through log for rewards.
     for key, value in log.rewards.items():
+        episode_rewards.append(np.sum(log.rewards[key]))
         episodic_reward_means.append(np.mean(log.rewards[key]))
 
+    #Iterate through log for other metrics.
     for key, value in log.episodic_metrics_variables.items():
         for i in range(0, len(log.episodic_metrics_variables[key]), 2):
             name = log.episodic_metrics_variables[key][i]
@@ -67,50 +95,51 @@ def showmetrics(log):
             if(name == "mean_q" and val != '--'):
                 episodic_mean_q.append(val)
 
-    #Running average
-    #episodic_reward_means = (np.cumsum(episodic_reward_means)  
-                                                #/ (np.arange(len(episodic_reward_means)) + 1))
 
-    plt.figure(1)
-    plt.subplot(311)
-    plt.title("Loss per Episode")
-    plt.plot(episodic_losses, 'b')
+    #Episodes represented per tick on plot.
+    window_length = 2
+    nb_episodes = len(episode_rewards)
 
-    plt.subplot(312)
-    plt.title("Mean Q Value per Episode")
-    plt.plot(episodic_mean_q, 'r')
+    #Partition episodes by window_length
+    nb_partitions = nb_episodes // window_length
+    episode_rewards = np.array(episode_rewards[:nb_partitions * window_length])
+    episode_rewards = episode_rewards.reshape((window_length, nb_partitions))
 
-    plt.subplot(313)
-    plt.title("Reward")
-    plt.plot(episodic_reward_means, 'g')
-    plt.show()
+    #Save rewards for future use.
+    filename = "data/{}/{}_episodes-rewards.npy".format(model_name, nb_episodes)
+
+    if not os.path.exists(os.path.dirname(filename)):
+        try:
+            os.makedirs(os.path.dirname(filename))
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+
+
+    np.save(filename, episode_rewards)
+
+    #Plot rewards.
+    sns.tsplot(data=episode_rewards)
+
+    plt.savefig("data/{}/{}_episodes-rewards.png".format(model_name, nb_episodes))
 
 def main(weights_file, options):
     #Initialize maze environments.
-    
-    #env = gym.make('MazeF4-v0')
-    #env2 = gym.make('MazeF1-v0')
-    #env3 = gym.make('MazeF2-v0')
-    #env4 = gym.make('MazeF3-v0')
-    #env5 = gym.make('Maze5-v0')
-    #env6 = gym.make('BMaze4-v0')
-
-    #envs = [env,env2,env3,env4,env5,env6]
-    
-    #env = gym.make('BMaze4-v0')
-    env = gym.make('IMaze3-v0')
+    #env = gym.make('IMaze3-v0')
+    env = gym.make('Taxi-v2')
 
     envs = [env]
     #Setting hyperparameters.
     nb_actions = env.action_space.n
-    e_t_size = 128
-    context_size = 128
-    nb_steps_warmup = int(5e2)
+    obs_dimensions = env.observation_space.n
+    e_t_size = 12
+    context_size = 12
+    nb_steps_warmup = int(5e3)
     nb_steps = int(4e6)
     buffer_size = 5e4
-    learning_rate = 25e-5
+    learning_rate = 1e-3
     target_model_update = 0.999
-    clipnorm = 20.
+    clipnorm = 1.
     switch_rate = 50
     window_length = 12
 
@@ -118,17 +147,21 @@ def main(weights_file, options):
     log = TrainEpisodeLogger()
     callbacks = [log]
 
-    #Initialize our MQN model.
+    #MQN model.
     
     #model = MQNmodel(e_t_size, context_size, window_length, nb_actions)
     #target_model = MQNmodel(e_t_size, context_size, window_length, nb_actions)
 
-    nb_atoms = 51
-    model = DistributionalMQNModel(e_t_size, context_size, window_length, nb_actions, nb_atoms)
-    target_model = DistributionalMQNModel(e_t_size, context_size, window_length, nb_actions, nb_atoms)
+
+    #Distributional MQN model.
+    nb_atoms = 6
+    v_min = -9.5
+    v_max = 20.5
+    model = DistributionalMQNModel(e_t_size, context_size, window_length, nb_actions, nb_atoms, obs_dimensions)
+    target_model = DistributionalMQNModel(e_t_size, context_size, window_length, nb_actions, nb_atoms, obs_dimensions)
     target_model.set_weights(model.get_weights())
     
-    #DQN
+    #DQN model
     #model = DQNmodel(nb_actions, window_length, input_shape=env.maze.matrix.shape)
     #target_model = DQNmodel(nb_actions, window_length, input_shape=env.maze.matrix.shape)
     #target_model.set_weights(model.get_weights())
@@ -148,6 +181,8 @@ def main(weights_file, options):
         nb_steps=7e4
     )
 
+    processor = TaxiProcessor()
+
     #Initialize and compile the DQN agent.
     '''
     dqn = DQNAgent(model=model, target_model=target_model, nb_actions=nb_actions, memory=experience,
@@ -159,18 +194,21 @@ def main(weights_file, options):
         model=model, 
         target_model=target_model,
         num_atoms=nb_atoms,
+        v_min=v_min,
+        v_max=v_max,
         nb_actions=nb_actions, 
         memory=experience,
         nb_steps_warmup=nb_steps_warmup, 
         target_model_update=target_model_update, 
         policy=policy,
+        processor=processor,
         batch_size=32
     )
 
     dqn.compile(Adam(lr=learning_rate,clipnorm=clipnorm),metrics=["mae"])
-    observation = env.reset()
+    #observation = env.reset()
     #print env.maze.matrix.shape
-    observation = np.reshape(observation, ((1,1,) + env.maze.matrix.shape))
+    #observation = np.reshape(observation, ((1,1,) + env.maze.matrix.shape))
     #print observation
     #print dqn.layers[1]
     #Load weights if weight file exists.
@@ -191,11 +229,16 @@ def main(weights_file, options):
     #Save weights.
     dqn.save_weights(weights_file)
     
-    #Visualization Tools
-    showmetrics(log)
-    visualizeLayer(dqn.model, dqn.layers[1], observation)
-    visualizeLayer(dqn.model, dqn.layers[2], observation)
-    visualizeLayer(dqn.model, dqn.layers[3], observation)
+    #Visualization / Logging Tools
+    model_name = weights_file.split(".")[0]
+    logmetrics(log, model_name)
+
+    #Debugging
+    if "debug" in options:
+        observation = env.reset()
+        outputLayer(dqn.model, np.array(experience[0].sample(32)[0].state0).reshape((1,12)))
+        #visualizeLayer(dqn.model, dqn.layers[1], observation)
+            
 
     return
 
@@ -204,7 +247,7 @@ if __name__ == "__main__":
     weights_file = None
     options = []
 
-    if len(sys.argv)  == 3:
+    if len(sys.argv)  >= 3:
         if(sys.argv[1].split('.')[-1] == "h5"):
             weights_file = sys.argv[1]
             options = sys.argv[2:]
